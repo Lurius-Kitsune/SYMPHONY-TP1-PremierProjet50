6580c6e4c9baa249169930ff53d28a0f8910073e
Lurius-Kitsune <LucasFox@outlook.fr>
2023-11-29 15:43:51 +0100

    - Creation du controller Home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,12 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/home', name: 'home_')]
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    #[Route('/homepage', name: 'homepage')]
+    public function homePage(): Response
     {
-        return $this->render('home/index.html.twig', [
+        return $this->render('home/homepage.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
